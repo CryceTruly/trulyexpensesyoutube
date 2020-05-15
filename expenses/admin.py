554f16aaff7da60cbd7e17b8fb42050df4740ccc
crycetruly <crycetruly@gmail.com>
2020-05-16 00:23:55 +0200

    ch(admin): Customize django admin

--- expenses/admin.py
+++ expenses/admin.py
@@ -3,5 +3,12 @@ from .models import Expense, Category
 # Register your models here.
 
 
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'category', 'date',)
+    search_fields = ('description', 'category', 'date',)
+
+    list_per_page = 5
+
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
